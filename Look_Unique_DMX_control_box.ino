a71a51a91115d1dc69db173e5e1ea130f92cf4e8
DrBit <betamaster50@gmail.com>
2015-08-03 12:18:36 +0200

    Code cleaned and optimized, added info

--- Look_Unique_DMX_control_box.ino
+++ Look_Unique_DMX_control_box.ino
@@ -1,5 +1,15 @@
+// Quim August 2015
+// Coded for Paradiso Amsterdam
+//
+// This code functions as a dmx controlled relay. In this case what we are 
+// switchin is the dmx data line itself to the next DMX connected device (Look Unique Hazer) 
+// By controlling the DMX we are also controlling when the HAer will automatically switch itself off
+// So by setting a DMX channel to control the machine we will ensure that the machin is properly 
+// shut down.
+
+
 /*
-  DMX_Slave.ino - Example code for using the Conceptinetics DMX library
+  Conceptinetics DMX library
   Copyright (c) 2013 W.A. van der Meeren <danny@illogic.nl>.  All right reserved.
 
   This library is free software; you can redistribute it and/or
@@ -35,75 +45,56 @@
 // shield to work
 //
 
-//
-// If the start address is for example 56, then the channels kept
-// by the dmx_slave object is channel 56-66
-//
-#define DMX_SLAVE_CHANNELS   1
-
-#define hz_IDLE 0
-#define hz_ON 1
-#define hz_count_down_OFF 2
-int hazer_mode = hz_IDLE;
-unsigned int Frame_received=0;
-
+// PIN setup
 const int chan_threshold = 25;    // Threshold in order to activate smoke (min. 1)
-const int Relay_PIN = 8;    // Pin conected at the relay
-const int TRPin = 2;  // Transmit enable / receive mode pin 0 = receive // 1 = transmit
-const int DMX_status = 9;
+const int Relay_PIN = 8;          // Pin conected at the relay
+const int TRPin = 2;              // Transmit enable / receive mode pin 0 = receive // 1 = transmit
+const int DMX_status = 9;         // Pin to show DMX status (led connected)
+const int Debug_switch = 12;      // PIN 12 is connected to switch but not used 
 
-const int Debug_switch = 12;    // PIN 12 is connected to switch but not used 
-
+// DMX Shield setup
+const int DMX_SLAVE_CHANNELS = 1; // Number of channels to read
+const int DMX_start_addres = 508; // Starting DMX address
 DMX_Slave dmx_slave ( DMX_SLAVE_CHANNELS );  // Configure a DMX slave controller
-int dmx_value = 0;
-boolean dmx_updated = false;
+int dmx_value = 0;                // Var to store DMX readed value
+boolean dmx_updated = false;      // Flag for signaling when a DMX frame has been received
 
+// Timing control
+boolean HazerShutDown = false;    // Flag to mark when the hazer is on shut down mode delayed
+volatile unsigned long lastFrameReceivedTime =0;      // Marks when the last DMX frame was received
+unsigned long Fader_OFF_Time;                         // Record the time fader whent off for calculating the timeout
+const unsigned long dmxTimeoutMillis = 10000UL;       // Timeout when we lose DMX connection - Time is set in ms (example: 10000UL = 10 sec)
+const unsigned long HazerOFFTimoeutMillis = 300000UL; // Time out when we switch smoke OFF - original -> 300000UL = 5 minutes
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-// timing
-boolean HazerShutDown = false;
-volatile unsigned long       lastFrameReceivedTime =0;
-unsigned long       Fader_OFF_Time;
-// Time is set in ms (example: 10000UL = 10 sec)
-const unsigned long dmxTimeoutMillis = 10000UL; // 10 seconds
-const unsigned long HazerOFFTimoeutMillis = 5000UL; // original -> 900000UL; // = 15 minutes
-boolean count_down_off = false;
-
-
-
-// the setup routine runs once when you press reset:
 void setup() { 
   pinMode ( TRPin , OUTPUT ); 
-  digitalWrite ( TRPin , LOW );  // Set to receive mode (my custom shield only)
-  pinMode ( DMX_status, OUTPUT ); // Set realy pin as output pin
+  digitalWrite ( TRPin , LOW );                           // Set to receive mode (my custom shield only)
+  pinMode ( DMX_status, OUTPUT );                         // Set realy pin as output pin
   digitalWrite (DMX_status, LOW);
-  pinMode ( Relay_PIN, OUTPUT ); // (relay is set to Default activated so when we activate relay it will disable the smoke machine)          
-  digitalWrite (Relay_PIN, HIGH); // By default the pin is disconnected so will be connected as soon as we receive the first package
-  // configurte pin to be as internall pull up input
-  pinMode(Debug_switch, INPUT_PULLUP); // for arduino IDE version 1.0.1 and above
-  dmx_slave.enable ();    // Enable DMX slave interface and start recording DMX data
-  dmx_slave.setStartAddress (1);  // Set start address to 1, this is also the default setting You can change this address at any time during the program
+  pinMode ( Relay_PIN, OUTPUT );                          // (relay is set to Default activated so when we activate relay it will disable the smoke machine)          
+  digitalWrite (Relay_PIN, HIGH);                         // By default the pin is disconnected so will be connected as soon as we receive the first package
+  pinMode(Debug_switch, INPUT_PULLUP);                    // configurte pin to be as internall pull up input for arduino IDE version 1.0.1 and above
+  dmx_slave.enable ();                                    // Enable DMX slave interface and start recording DMX data
+  dmx_slave.setStartAddress (DMX_start_addres);           // Set start address to 1, this is also the default setting You can change this address at any time during the program
   dmx_slave.onReceiveComplete ( OnFrameReceiveComplete ); // Call back function when a frame is received
-  startingdevice ();
+  startingdevice ();                                      // Signals starting procedure
 }
 
-
-
-// the loop routine runs over and over again forever:
 void loop() 
 {
-  if (dmx_updated) {
+  if (dmx_updated) {                  // If a new DMX frame has been received
     analogWrite (DMX_status, dmx_value);
-    if ( dmx_value > chan_threshold ) { 
-      DMX_to_HAZER_ON ();
+    if ( dmx_value > chan_threshold ) {   // If DMX value is grater than the preset threshold 
+      DMX_to_HAZER_ON ();             // Switch smoke on
     }else{
-      DMX_to_HAZER_OFF_DELAYED ();
+      DMX_to_HAZER_OFF_DELAYED ();    // Switch smoke off
     }
     dmx_updated = false;
   }
 
-  if (isHazerON && HazerShutDown ) {      // Cheking timer
+  if (isHazerON && HazerShutDown ) {  // Cheking timer
     if ((millis() - Fader_OFF_Time) > HazerOFFTimoeutMillis) {    // check if time has passed 
       DMX_to_HAZER_OFF ();
     }
@@ -111,33 +102,33 @@ void loop()
 
   // If we didn't receive a DMX frame within the timeout period  clear all dmx channels  
   if ((millis() - lastFrameReceivedTime) > dmxTimeoutMillis ) {  
-    dmx_slave.getBuffer().clear();  // Clear lib data
-    dmx_value = 0;    // clear local data
-    dmx_updated = true;   // Update data 1 time
-    // Since we are not receiving more packets we will never go into function So we have to controll all from here
-    DMX_to_HAZER_OFF_DELAYED ();    // Start shut down procedure
+    dmx_slave.getBuffer().clear();    // Clear lib data
+    dmx_value = 0;                    // clear local data
+    dmx_updated = true;               // Update data 1 time (as if we received a DMX fram)
+    // Since we are not receiving more packets we will never go into function smoke shut dowm So we have to controll all from here
+    DMX_to_HAZER_OFF_DELAYED ();      // Start shut down procedure
   }
 }
 
 void OnFrameReceiveComplete (void) {
   lastFrameReceivedTime = millis ();
   dmx_value = dmx_slave.getChannelValue (1);
-  dmx_updated = true;
+  dmx_updated = true;                 // Flag that a frame has been received
 }
 
 void DMX_to_HAZER_OFF () {
-  digitalWrite (Relay_PIN, HIGH); // Set DMX to hazer HIGH (meaning disconected) 
+  digitalWrite (Relay_PIN, HIGH);     // Set DMX to hazer HIGH (meaning disconected) 
 }
 
 void DMX_to_HAZER_OFF_DELAYED () {
-  if (!HazerShutDown && isHazerON){    // We doit only one time or the dealy mark  (Fader_OFF_Time) would keep updating
-    HazerShutDown = true;
-    Fader_OFF_Time = millis (); // Record the time fader whent off for calculating the timeout.
+  if (!HazerShutDown && isHazerON){   // We do it only one time or the delay mark  (Fader_OFF_Time) would keep updating
+    HazerShutDown = true;             // Flag to mark when the hazer is on shut down mode delayed
+    Fader_OFF_Time = millis ();       // Record the time fader whent off for calculating the timeout.
   }
 }
 
 void DMX_to_HAZER_ON () {
-  digitalWrite (Relay_PIN, LOW); // Set DMX to hazer LOW (meaning connected)
+  digitalWrite (Relay_PIN, LOW);      // Set DMX to hazer LOW (meaning connected)
   HazerShutDown = false;
 }
 
@@ -147,13 +138,14 @@ boolean isHazerON () {
 }
 
 void startingdevice () {
-  digitalWrite (DMX_status, HIGH);  // Set led HIGH 
-  delay(500);
-  digitalWrite (DMX_status, LOW);  // Set led LOW 
-  delay(500);
-  digitalWrite (DMX_status, HIGH);  // Set led HIGH 
-  delay(500);
-  digitalWrite (DMX_status, LOW);  // Set led LOW
+  // Blink the DMX status led to signal the start
+  int N_blink_times = 4;
+  for (int a = 0; a<N_blink_times; a++) {
+    digitalWrite (DMX_status, HIGH);    // Set led HIGH 
+    delay(100);
+    digitalWrite (DMX_status, LOW);     // Set led LOW 
+    delay(100);
+  }
 
   // If debug button is pressed (reading LOW) we enter debug mode
   int debug_status = digitalRead (Debug_switch);
@@ -161,59 +153,3 @@ void startingdevice () {
     //entering debug mode
   }
 }
-
-
-
- /*
-  if ((millis() - lastFrameReceivedTime) > dmxTimeoutMillis ) {  // If we didn't receive a DMX frame within the timeout period  clear all dmx channels  
-    dmx_slave.getBuffer().clear();
-    // Since we are not receiving more packets we will never go into function
-    // So we have to controll all from here
-    DMX_to_HAZER_OFF ();
-    count_down_off = false;
-    hazer_mode = hz_IDLE;
-  }
-
-  if (count_down_off) {
-    if ((millis() - Fader_OFF_Time) > HazerOFFTimoeutMillis) {    // check if time has passed 
-      DMX_to_HAZER_OFF ();
-      count_down_off = false;
-      hazer_mode = hz_IDLE;
-    }
-  }
-  */
-  
-
-  /*
-  switch (hazer_mode) {
-    case hz_IDLE: {   // wait until we get the data.
-      if (Frame_received >0) {
-        if (IsDMXON ()) {
-          hazer_mode = hz_ON;
-          DMX_to_HAZER_ON ();
-        }else{
-          DMX_to_HAZER_OFF ();
-        }
-      }
-    break;}
-    
-    case hz_ON: {
-      if (Frame_received >0) {
-        if (!IsDMXON ()) {
-          hazer_mode = hz_count_down_OFF;
-          count_down_off = true;
-        }
-      }
-    break;}
-    
-    case hz_count_down_OFF: {
-      if (Frame_received >0) {
-        if (IsDMXON ()) {   
-          count_down_off = false;
-          hazer_mode = hz_ON;
-          DMX_to_HAZER_ON ();
-        }
-      }
-    break;}
-  } 
-  */
