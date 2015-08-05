51d64057831918d5452876ef6c4d4528dd98b79e
DrBit <betamaster50@gmail.com>
2015-08-05 21:26:07 +0200

    Info updated

--- Look_Unique_DMX_control_box.ino
+++ Look_Unique_DMX_control_box.ino
@@ -3,17 +3,6 @@
 // Coded for Paradiso Amsterdam
 //
 // For use with the Conceptinetics DMX Isolated shield.
-//
-// This piece of code has been designed to work with a custome made shield on top of the Conceptinetics
-// Shield. Refer to schematics for further info.
-// The main functionality is to listen to a DMX channel and connect or disconnect DMX signal to an attached 
-// device, in this case th Look Unique Hazer.
-// Since the lack of control channel on the Unique hazer this will act as one with as less as possible
-// modifications on the original hardware.
-
-
-// I'm using DMX library from Conceptinetics with slight modification to be compatible with the 
-// new Arduino IDE. (attached in this repository )
 
 /*
   Conceptinetics DMX library
